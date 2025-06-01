74be30441f9e6d4b2d47b5b718bce86a94d48e23
Lens <yoanyombapro@gmail.com>
2025-05-31 23:22:12 -0400

    feat: wip
    Signed-off-by: Lens <yoanyombapro@gmail.com>

--- apps/www-prospector/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
-    },
-    output: 'standalone',
-};
-
-export default nextConfig;
--- apps/www-prospector/types/analytics.ts
+++
@@ -1,1 +0,0 @@
-export type Period = { month: number; year: number }
